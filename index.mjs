8f5bc840f94776be22437d77f476a00272b3a031
Mario Sclavo <mmsclavo@gmail.com>
2023-10-31 14:04:56 -0300

    Corrigindo o retorno da lambda jwt

--- index.mjs
+++ index.mjs
@@ -6,38 +6,39 @@ import jwt from 'jsonwebtoken';
 
 const jwtSecret = 'sua-chave-secreta-para-o-JWT';
 
-export const handler =  function(event, context, callback) {
-  const userId = event.userId; // Suponha que você recebe o ID do usuário na entrada do evento
+export const handler = function (event, context, callback) {
+    const userId = event.userId; // Suponha que você recebe o ID do usuário na entrada do evento
 
-  try {
-    // const params = {
-    //   TableName: 'NomeDaSuaTabelaNoDynamoDB',
-    //   Key: {
-    //     userId: userId, // Substitua com a chave primária apropriada
-    //   },
-    // };
+    try {
+        // const params = {
+        //   TableName: 'NomeDaSuaTabelaNoDynamoDB',
+        //   Key: {
+        //     userId: userId, // Substitua com a chave primária apropriada
+        //   },
+        // };
 
-    // const { Item } = await dynamodb.get(params).promise();
+        // const { Item } = await dynamodb.get(params).promise();
 
-    // if (!Item) {
-    //   throw new Error('Usuário não encontrado');
-    // }
+        // if (!Item) {
+        //   throw new Error('Usuário não encontrado');
+        // }
 
-    const token = jwt.sign({ userId: userId }, jwtSecret, { expiresIn: '1h' });
+        const token = jwt.sign({ userId: userId }, jwtSecret, { expiresIn: '1h' });
 
-    console.log({
-        statusCode: 200,
-        body: JSON.stringify({ token }),
-      });
-
-    return {
-      "statusCode": 200,
-      "body": JSON.stringify({ token }),
-    };
-  } catch (error) {
-    return {
-      "statusCode": 500,
-      "body": JSON.stringify({ error: error.message }),
-    };
-  }
+        callback(null, {
+            "statusCode": 200,
+            "body": JSON.stringify({ token }),
+            "headers": {
+                "Content-Type": "application/json"
+            }
+        });
+    } catch (error) {
+        callback(null, {
+            "statusCode": 500,
+            "body": JSON.stringify({ error, error.message }),
+            "headers": {
+                "Content-Type": "application/json"
+            }
+        });
+    }
 };
