70709d5a14aa3f3fe800c0e25441152d1ad4b914
Mario Sclavo <mmsclavo@gmail.com>
2023-11-01 21:44:00 -0300

    feature: incluida consulta ao banco

--- index.mjs
+++ index.mjs
@@ -1,18 +1,29 @@
 // const AWS = require('aws-sdk');
 // const jwt = require('jsonwebtoken');
 import jwt from 'jsonwebtoken';
+import pg from 'pg';
 
 // const dynamodb = new AWS.DynamoDB.DocumentClient();
 
+const pool = new Pool({
+    user: 'ademar',
+    host: 'terraform-20231101232846266700000001.cbwfn3u5fvvq.us-east-1.rds.amazonaws.com',
+    database: 'fiap_projeto',
+    password: 'pagodaodamassa123',
+    port: 5432, // Porta padrão do PostgreSQL
+});
+
 const jwtSecret = 'sua-chave-secreta-para-o-JWT';
 
-export const handler = function (event, context, callback) {
+export const handler = async function (event, context, callback) {
 
     console.log(`event: ${JSON.stringify(event)}`);
     console.log(`context: ${JSON.stringify(context)}`);
 
     const payload = JSON.parse(event.body);
     const cpf = payload.cpf;
+
+    const client = await pool.connect();
 
     try {
         // const params = {
@@ -28,11 +39,25 @@ export const handler = function (event, context, callback) {
         //   throw new Error('Usuário não encontrado');
         // }
 
-        const token = jwt.sign({ userId: cpf }, jwtSecret, { expiresIn: '1h' });
+        const result = await client.query('SELECT * FROM clientes c where c.cpf = $1', [cpf]);
+        console.log('Resultados da consulta:', result.rows);
+
+        if (result.rows.length === 0) {
+            callback(null, {
+                statusCode: 404,
+                body: 'Nenhum cliente encontrado com o CPF fornecido.',
+                headers: {
+                    "Content-Type": "application/text"
+                }
+            });
+        }
+
+        const codigoCliente = result.rows[0].codigo;
+        const token = jwt.sign({ userId: codigoCliente }, jwtSecret, { expiresIn: '1h' });
 
         callback(null, {
             statusCode: 200,
-            body: JSON.stringify({ token: token }) ,
+            body: JSON.stringify({ token: token }),
             headers: {
                 "Content-Type": "application/json"
             }
@@ -45,5 +70,7 @@ export const handler = function (event, context, callback) {
                 "Content-Type": "application/json"
             }
         });
+    } finally {
+        client.release(); // Libera a conexão de volta para o pool
     }
 };
