627ba951bb4c60a38941ee5b4f045179800748bd
Mario Sclavo <mmsclavo@gmail.com>
2023-11-04 20:24:20 -0300

    fix: pool de conexao

--- index.mjs
+++ index.mjs
@@ -1,34 +1,33 @@
 import jwt from 'jsonwebtoken';
 import pkg from 'pg';
 
+const { Pool } = pkg;
 const BD_USER = process.env.BD_USER;
 const BD_PASSWORD = process.env.BD_PASSWORD;
 const BD_HOST = process.env.BD_HOST;
 const BD_PORT = process.env.BD_PORT;
 const BD_DATABASE = process.env.BD_DATABASE;
-
 const QUERY_BUSCA_CLIENT_ID = 'SELECT * FROM clientes WHERE cpf = $1::text';
-
-const { Pool } = pkg;
-const pool = new Pool({
-    user: BD_USER,
-    password: BD_PASSWORD,
-    host: BD_HOST,
-    port: BD_PORT,
-    database: BD_DATABASE,
-    max: 1,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export const handler = async function (event, context, callback) {
     const payload = JSON.parse(event.body);
     const cpf = payload.cpf;
+
+    const pool = new Pool({
+        user: BD_USER,
+        password: BD_PASSWORD,
+        host: BD_HOST,
+        port: BD_PORT,
+        database: BD_DATABASE,
+        max: 1,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
     const client = await pool.connect();
 
     try {
