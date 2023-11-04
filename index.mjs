b3c3fd6a6934aa16bc10467d0c795c73b9ebfabf
Mario Sclavo <mmsclavo@gmail.com>
2023-11-04 19:59:12 -0300

    refactor: incluido constants para as configurações

--- index.mjs
+++ index.mjs
@@ -1,15 +1,21 @@
-// const AWS = require('aws-sdk');
 import jwt from 'jsonwebtoken';
 import pkg from 'pg';
 
-const { Pool } = pkg;
+const BD_USER = process.env.BD_USER;
+const BD_PASSWORD = process.env.BD_PASSWORD;
+const BD_HOST = process.env.BD_HOST;
+const BD_PORT = process.env.BD_PORT;
+const BD_DATABASE = process.env.BD_DATABASE;
 
+const QUERY_BUSCA_CLIENT_ID = 'SELECT * FROM clientes WHERE cpf = $1::text';
+
+const { Pool } = pkg;
 const pool = new Pool({
-    user: 'fiap_user',
-    host: 'projeto-fiap-db.c8jiyjlno5mw.us-east-1.rds.amazonaws.com',
-    database: 'fiap_projeto',
-    password: 'fiap_course1234',
-    port: 5432,
+    user: BD_USER,
+    password: BD_PASSWORD,
+    host: BD_HOST,
+    port: BD_PORT,
+    database: BD_DATABASE,
     max: 1,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
@@ -23,42 +29,31 @@ const JWT_SECRET = process.env.JWT_SECRET;
 export const handler = async function (event, context, callback) {
     const payload = JSON.parse(event.body);
     const cpf = payload.cpf;
-    const client = await pool.connect();
+    // const client = await pool.connect();
 
     try {
-        const result = await client.query('SELECT * FROM clientes WHERE cpf = $1::text', [cpf]);
+        const result = await client.query(QUERY_BUSCA_CLIENT_ID, [cpf]);
         console.log('Resultados da consulta:', result.rows);
 
         if (result.rows.length === 0) {
-            callback(null, {
-                statusCode: 404,
-                body: JSON.stringify({ message: 'Nenhum cliente encontrado com o CPF fornecido.' }),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
+            callback(null, makeResponse(404, {message: 'Nenhum cliente encontrado com o CPF fornecido.'}) );
         } else {
-            const codigoCliente = result.rows[0].codigo;
-            const token = jwt.sign({ userId: codigoCliente }, JWT_SECRET, { expiresIn: '1h' });
-
-            callback(null, {
-                statusCode: 200,
-                body: JSON.stringify({ token: token }),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
+            const payload = {codigo: result.rows[0].codigo, nome: result.rows[0].nome, email: result.rows[0].email};
+            const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+            callback(null, makeResponse(200, {token: token}));
         }
     } catch (error) {
-        callback(null, {
-            statusCode: 500,
-            body: JSON.stringify({ error: error.message }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
+        callback(null, makeResponse(500, {error: error.message}));
     } finally {
-        client.release(); // Libera a conexão de volta para o pool
+        // client.release();
         await pool.end();
     }
 };
+
+function makeResponse(code, body, header = null) {
+    return {
+        statusCode: code,
+        body: JSON.stringify(body),
+        headers: header ? header : {"Content-Type": "application/json"}
+    }
+}
